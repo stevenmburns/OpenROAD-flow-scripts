a7c1aaafde6792153906707e8df0abe8974d4a9a
Steven Burns <smburns47@gmail.com>
2022-11-09 10:26:20 -0800

    Added generated adder examples for nangate

---
+++ flow/designs/nangate45/brent_kung_256/config.mk
@@ -0,0 +1,14 @@
+export DESIGN_NICKNAME = brent_kung_256
+export DESIGN_NAME = Adder
+export PLATFORM    = nangate45
+
+export VERILOG_FILES = ./designs/src/$(DESIGN_NICKNAME)/Adder.sv
+export SDC_FILE      = ./designs/$(PLATFORM)/$(DESIGN_NICKNAME)/constraint.sdc
+
+# These values must be multiples of placement site
+# x=0.19 y=1.4
+export DIE_AREA    = 0 0 190 70
+export CORE_AREA   = 9.5 11.2 180.5 58.8
+export PLACE_PINS_ARGS = -exclude left:* -exclude right:*
+
+export PLACE_DENSITY_LB_ADDON = 0.20
